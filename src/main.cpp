71f36b689b1224928b56020b8f16bf9943003c17
gtchandra <gtchandra@me.me>
2018-10-29 10:54:55 +0100

    added WiFiManager Library

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,19 @@
 #include <TM1637Display.h>
 #include <stdint.h>
 #include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+
 void conTimeService();
 const int CLK = D6; //Set the CLK pin connection to the display
 const int DIO = D5; //Set the DIO pin connection to the display
 String ipAddress;
 void updateClk();
+bool timeSet=false;
 
-//next step add: https://github.com/tzapu/WiFiManager/blob/master/WiFiManager.cpp
-//------- WiFi Settings -------
-char ssid[] = "Vodafone-56413954";       // your network SSID (name)
-char password[] = "12345678";  // your network key
 
 String host = "worldclockapi.com";
 int numCounter = 0;
@@ -19,14 +22,12 @@ uint8_t data[] = {0x0, 0x0, 0x0, 0x0};
 uint8_t digit0,digit1,digit2,digit3 =0;
 
 TM1637Display display(CLK, DIO); //set up the 4-Digit Display.
-//WiFiClientSecure client;
 WiFiClient client;
 int innerTime=0;
 long checkTime=0;
 bool colon=0;
 
 class Clock {
-
  public:
     Clock();
     String getHours();
@@ -83,37 +84,12 @@ Clock myClock=Clock();
 
 void setup()
 {
- myClock.setHours(23);
- myClock.setMinutes(58);
- Serial.begin(9600);
+ myClock.setHours(0);
+ myClock.setMinutes(0);
  display.setBrightness(0x0f); //set the diplay to maximum brightness
- // Set WiFi to station mode and disconnect from an AP if it was Previously
- // connected
- WiFi.mode(WIFI_STA);
- WiFi.disconnect();
- delay(100);
- // Attempt to connect to Wifi network:
- Serial.print("Connecting Wifi: ");
- Serial.println(ssid);
- WiFi.begin(ssid, password);
- int timeout=0;
- while ((WiFi.status() != WL_CONNECTED) and (timeout<100)) {
-   Serial.print(".");
-   Serial.print(WiFi.status());
-   delay(500);
-   timeout++;
- }
- if (WiFi.status() == WL_CONNECTED){
-   Serial.println("");
-   Serial.println("WiFi connected");
-   display.setBrightness(0x07);
-   Serial.println("IP address: ");
-   IPAddress ip = WiFi.localIP();
-   Serial.println(ip);
-   ipAddress = ip.toString();
-   conTimeService();
-  }
-  else Serial.println("WiFi connection FAILED");
+ Serial.begin(9600);
+ WiFiManager wifiManager;
+ wifiManager.autoConnect();
 }
 
 void conTimeService ()
@@ -122,7 +98,6 @@ void conTimeService ()
     if (client.connect(host,80)) {
     client.println("GET /api/json/cet/now HTTP/1.1");
     client.println("Host:"+host);
-    client.println("");
     //client.print("Content-Length: ");
     //client.println(PostData.length());
     client.println();
@@ -144,20 +119,36 @@ void conTimeService ()
     // from the server, read them and print them:
     String c="";
     String t="";
+    String json="";
+    StaticJsonBuffer<400> jsonBuffer;
     while(client.available())
     {
-    if (client.available()) {
-      c = client.readStringUntil('\r');
-      Serial.print(c);
-      if (c.indexOf('currentDayTime')>0) t=c.substring(c.indexOf('currentDayTime')+25,c.indexOf('currentDayTime')+30);
-      }
+        json = client.readStringUntil('\r\n');
+        Serial.println("content="+json);
+        if (json.indexOf("{")==0) {
+            JsonObject& root = jsonBuffer.parseObject(json);
+            if (root.success()) {
+                String currentTime=root["currentDateTime"];
+                Serial.println("current time:"+currentTime);
+                t="";
+                t=currentTime.substring(currentTime.indexOf('T')+1,currentTime.indexOf('T')+6);
+                Serial.println("current t:"+t);
+            }
+            else Serial.println("failed to parse!");
+        }
+
+        //if (c.indexOf('currentDayTime')>0) t=c.substring(c.indexOf('currentDayTime')+25,c.indexOf('currentDayTime')+30);
     }
-    myClock.setHours(t.substring(0,2).toInt());
-    myClock.setMinutes(t.substring(3,5).toInt());
+    //include additional checks to avoid corrupted reads
+    if (t!="") {
+        myClock.setHours(t.substring(0,2).toInt());
+        myClock.setMinutes(t.substring(3,5).toInt());
+        timeSet=true;
+      }
     }
     else
     {
-      Serial.println("client did not connect");
+      Serial.println("client failed to connect");
     }
 }
 
@@ -175,31 +166,20 @@ void updateClk() {
     data[3]= display.encodeDigit(myClock.getMinutes()[1]);
     display.setSegments(data);
 }
+
 void loop()
 {
-  //conTimeService();
-
   if ((millis()-innerTime)>999)
   {
     updateClk();
     innerTime=millis();
+    if(!timeSet) conTimeService();
   }
-  if ((millis()-checkTime)>360000) {
-    checkTime=millis();
-    conTimeService();
-  }
-  /*
-  {
-    colon=!colon;
-    innerTime=millis();
-    myClock.tick();
-    Serial.println("Time:"+myClock.getHours()+":"+myClock.getMinutes()+":"+myClock.getSeconds());
-    data[0]= display.encodeDigit(myClock.getHours()[0]);
-    if (colon) data[1]= 0x80|display.encodeDigit(myClock.getHours()[1]);
-    else data[1]= display.encodeDigit(myClock.getHours()[1]);
-    data[2]= display.encodeDigit(myClock.getMinutes()[0]);
-    data[3]= display.encodeDigit(myClock.getMinutes()[1]);
-    display.setSegments(data);
-  }*/
 
+  if ((millis()-checkTime)>3600000) {
+    //wake WiFi
+    conTimeService();
+    //shutdown wifi
+    checkTime=millis();
+  }
 }
